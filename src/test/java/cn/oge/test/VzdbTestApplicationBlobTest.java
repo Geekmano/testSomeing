cb4f91b21b84a678e01784f546054c63c7630ce4
mall <389489663@qq.com>
2016-11-22 10:27:55 +0800

    update config

--- src/test/java/cn/oge/test/VzdbTestApplicationBlobTest.java
+++ src/test/java/cn/oge/test/VzdbTestApplicationBlobTest.java
@@ -101,9 +101,10 @@ public class VzdbTestApplicationBlobTest {
 	 * @return
 	 */
 	@Test
+	@Ignore
 	public void testBlobTag() {
 				// 添加源编码
-//				addBlobTag();
+				addBlobTag();
 
 				 // 查询源编码
 				BlobPara[] floatParas = assertBlobResult(config.getBlobTags());
